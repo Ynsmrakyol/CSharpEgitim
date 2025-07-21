d7eda04a51f93779e45017bfba9175779365d02a
Yunus Emre Akyol <103356562+Ynsmrakyol@users.noreply.github.com>
2025-07-21 19:57:03 +0300

    Add files via upload

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If Else
+            //Console.Write("Lütfen Şifreyi giriniz: ");
+            //string password;
+            //password = Console.ReadLine();
+            //if (password =="ABCD")
+            //{
+            //    Console.WriteLine("Şifreniz Doğru!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre Yalnış");
+            //}
+
+            //string capital, country;
+            //Console.Write("Başkenti Giriniz ");
+            //capital = Console.ReadLine();
+            //Console.Write("Ülkeyi giriniz: ");
+            //country = Console.ReadLine();
+
+            //if (capital == "ankara" & country == "Türkiye")
+            //{
+            //    Console.Write("Veriler Doğrulandı");
+            //}
+            //else
+            //{
+            //    Console.Write("Hatalı veri girişi");
+            //}
+
+            //int number;
+            //Console.Write("Sayıyı giriniz: ");
+            //number=int.Parse(Console.ReadLine());
+            //if (number == 5)
+            //{
+            //    Console.WriteLine("Sayı doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı hatalı");
+            //}
+
+            //int exam1, exam2, exam3, average;
+            //string result;
+
+            //Console.Write("1.Sınav:");
+            //exam1=int.Parse(Console.ReadLine());
+            //Console.Write("2.Sınav:");
+            //exam2=int.Parse(Console.ReadLine());
+            //Console.Write("3.Sınav:");
+            //exam3 =int.Parse(Console.ReadLine());
+            //average=(exam1+exam2+exam3)/3;
+            //Console.WriteLine("Ortalamanız: "+average);
+
+            //if (average <= 50 & average>=0)
+            //{
+            //    result = "Sonuç Vasat";
+            //}
+            //if (average <= 70 & average > 50)
+            //{
+            //    result = "Sonuç Orta";
+            //}
+            //if (average <= 84 & average > 70)
+            //{
+            //    result = "Sonuç İyi";
+
+            //}
+            //if (average <= 100 & average > 84)
+            //{
+            //    result = "Sonuç Çok iyi";
+            //}
+            //else
+            //{
+            //    result = "Hatalı Giriş!";
+            //}    Console.WriteLine(result);
+            //Console.Read();
+
+            //string City;
+            //Console.Write("Lütfen Şehir Girişi Yapın: ");
+            //City = Console.ReadLine();
+
+
+            //if (City == "Adana" | City == "Samsun" | City == "İstanbul" | City == "Balıkesir")
+            //{
+            //    Console.WriteLine("Şehir Mevcut");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şehir Mevcut Değil");
+            //}
+
+
+            //Console.Write("Lütfen kullanıcı adını giriniz: ");
+            //string username = Console.ReadLine();
+            //if (username != "admin")
+            //{
+            //    Console.WriteLine("Bu kullanıcı adı kabul edilemez.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hoşgeldiniz");
+            //}
+
+
+
+
+
+
+            //Console.Read();
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Mod İşlemleri 
+
+            //int number;
+            //number = 26;
+            //int result = number % 5;
+            //Console.Write(result);
+
+
+
+
+            //Console.Read();
+
+
+            #endregion
+
+            #region Switch Case
+
+            //Console.Write("Lütfen ay girişi yapınız: ");
+            //int monthNumber=int.Parse(Console.ReadLine());
+
+            //switch(monthNumber)
+            //{
+            //    case 1:Console.WriteLine("Ocak"); break;
+            //    case 2:Console.WriteLine("Şubat");break;
+            //    case 3:Console.WriteLine("Mart");break;
+            //    case 4:Console.WriteLine("Nisan");break;
+            //    case 5:Console.WriteLine("Mayıs"); break;
+            //    case 6:Console.WriteLine("Haziran"); break;
+            //    case 7:Console.WriteLine("Temmuz"); break;
+            //    case 8:Console.WriteLine("Ağustos"); break;
+            //    case 9:Console.WriteLine("Eylül"); break;
+            //    case 10:Console.WriteLine("Ekim"); break;
+            //    case 11:Console.WriteLine("Kasım"); break;
+            //    case 12:Console.WriteLine("Aralık"); break;
+            //    default: Console.WriteLine("Hatalı giriş"); break;
+            //}
+
+            //Console.Read();
+
+
+            #endregion
+
+            #region SwitchCase Hesap Makinesi
+
+            //int number1,number2,result;
+            //char symbol;
+            //Console.Write("İlk Sayıyı Giriniz: ");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.Write("İkinci Sayıyı Giriniz:");
+            //number2=int.Parse(Console.ReadLine());
+            //Console.Write("Lütfen Yapmak İstediğiniz İşlemi Giriniz");
+            //symbol=char.Parse(Console.ReadLine());
+            //result = 0;
+
+            //switch (symbol)
+            //{
+            //    case '+':
+            //        result=number1+ number2; break;
+            //    case '-':
+            //        result=number1- number2; break;
+            //    case '*':
+            //        result=number1* number2; break;
+            //    case '/':
+            //        result=number2/ number1; break;
+            //    default : Console.WriteLine("Geçerli bir sembol değil!"); break;
+            //}
+            //Console.WriteLine(result);
+
+
+            //Console.Read();
+            #endregion
+
+
+
+
+        }
+    }
+}
